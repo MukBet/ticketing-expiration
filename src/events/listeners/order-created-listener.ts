307fc1376fe38069b6b126a386133797eb4f3bd0
MukBet <mukbet@users.noreply.github.com>
2026-01-26 22:00:57 +0200

    fixes before run the service

--- src/events/listeners/order-created-listener.ts
+++ src/events/listeners/order-created-listener.ts
@@ -1,35 +1,16 @@
 import { Listener, OrderCreatedEvent, Subjects } from '@motway_ticketing/common';
 import { queueGroupName } from './queue-group-name';
 import { Message } from 'node-nats-streaming';
-import { Ticket } from '../../models/ticket';
-import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
-
+import { expirationQueue } from '../../queues/expiration-queues';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-    console.log('OrderCreatedListener Event data!', data);
-
-    const ticket = await Ticket.findById(data.ticket.id);
-
-    if (!ticket) {
-      throw new Error('Ticket not found');
-    }
-
-    ticket.set({ orderId: data.id });
-    await ticket.save();
-
-    await new TicketUpdatedPublisher(this.client).publish({
-      id: ticket.id,
-      title: ticket.title,
-      price: ticket.price,
-      userId: ticket.userId,
-      orderId: ticket.orderId,
-      version: ticket.version
+    await expirationQueue.add({
+      orderId: data.id,
     });
-
     msg.ack();
   }
 }
